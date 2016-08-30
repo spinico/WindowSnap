53dc97d3e0438c08301fd073f15bea242a84a138
Nicolas Spiric <nspiric@nspx.ca>
2016-08-30 13:57:54 -0400

    Fixed minor namespace issue

--- Demo/App.xaml.cs
+++ Demo/App.xaml.cs
@@ -2,10 +2,5 @@
 {
     using System.Windows;
 
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    public partial class App : Application { }
 }
